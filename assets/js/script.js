f0868dfe229ad49f1065f9d9971aa05fc94c9e0f
FranKBrownn <Frankcdbrown97@gmail.com>
2022-06-07 09:02:14 +0000

    coded the subtract game mode to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,9 +23,14 @@ function runGame(gameType){
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
     } 
-    else if (gameType === "multiply") {
+    else if (gameType === "multiply"){
+
         displayMultiplyQuestion(num1, num2)
-    }
+
+    } 
+    else if (gameType === "subtract")
+
+    displaySubtractionQuestion(num1, num2)
     else{
         alert(`unknown game type : ${gameType}`)
         throw `unknown game type : ${gameType}. Aborting!` ;
@@ -58,7 +63,11 @@ function calculateCorrectAnswer() {
     } 
     else if (operator === "x"){
         return [operand1 * operand2 , "multiply"]
+    } 
+    else if (operator === "-"){
+        return [operand1 - operand2 , "subtract"]
     }
+
     else {
         alert(`umimplemented operator ${operator}`);
         throw `umimplemented operator ${operator}. Aborting `
@@ -82,11 +91,14 @@ function displayAdditionQuestion(operand1, operand2){
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
 }
-function displaySubtractionQuestion(){
-
+function displaySubtractionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 function displayDivisionQuestion(){
 
+    
 }
 
 function displayMultiplyQuestion(operand1, operand2){
