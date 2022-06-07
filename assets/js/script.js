61734d660babc28fb162d5df9b3e9f5828f7726c
FranKBrownn <Frankcdbrown97@gmail.com>
2022-06-07 01:44:47 +0000

    checked to see if the buttons were resposive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
-console.log("conected")
+ document.addEventListener("DOMContentLoaded", function(){
+     let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click" , function(){
+            if(this.getAttribute("data-type") === "submit") {
+                alert("you clicked submit!")
+            }
+            else {
+                let gameType = this.getAttribute("data-type")
+                alert(`you click ${gameType}`)
+            }
+        })
+
+    }
+})
+
+function runGame(){
+
+}
+function checkAwnser(){
+
+}
+function calculateCorrectAwnser() {
+    
+}
+function incrementScore(){
+
+}
+function incrementWrongAwnser(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractionQuestion(){
+
+}
+function displayDivisionQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
