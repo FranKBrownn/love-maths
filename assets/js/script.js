6d2bc30de5440fc6e3fceacdadce9430cc59c146
FranKBrownn <Frankcdbrown97@gmail.com>
2022-06-07 02:03:43 +0000

    created the addition gamemode

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,24 @@
             }
             else {
                 let gameType = this.getAttribute("data-type")
-                alert(`you click ${gameType}`)
+                runGame(gameType)
             }
         })
 
     }
+    runGame("addition");
 })
 
-function runGame(){
+function runGame(gameType){
+    let num1 = Math.floor(Math.random() * 25) + 1
+    let num2 = Math.floor(Math.random() * 25) + 1
 
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else{
+        alert(`unknown game type : ${gameType}`)
+        throw `unknown game type : ${gametype}. Aborting!` ;
+    }
 }
 function checkAwnser(){
 
@@ -30,8 +39,11 @@ function incrementScore(){
 function incrementWrongAwnser(){
 
 }
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 function displaySubtractionQuestion(){
 
